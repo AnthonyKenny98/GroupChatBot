4cd0fe9f80c3ad5b81ebda2b8bdddabefe32457a
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2019-11-09 11:33:14 -0500

    Add tests

---
+++ src/tests/sample_test.py
@@ -0,0 +1,11 @@
+"""Sample Test."""
+# -*- coding: utf-8 -*-
+# @Author: AnthonyKenny98
+# @Date:   2019-11-09 11:32:19
+# @Last Modified by:   AnthonyKenny98
+# @Last Modified time: 2019-11-09 11:32:35
+
+
+def test_sample1():
+    """Sample Test."""
+    assert 1 == 1
