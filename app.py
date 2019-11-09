a96027ac5e4abcb72b556423f0eeb694357fd4cc
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2019-11-09 15:45:11 -0500

    heroku commit

---
+++ app.py
@@ -0,0 +1,18 @@
+"""App.py."""
+# -*- coding: utf-8 -*-
+# @Author: AnthonyKenny98
+# @Date:   2019-11-09 15:25:47
+# @Last Modified by:   AnthonyKenny98
+# @Last Modified time: 2019-11-09 15:29:11
+
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def respond():
+    """Basic Respond."""
+    return jsonify({"Message": "Response"})
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
