5764adcc22cc6f64ccc41097a3537702a4b72bc5
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2019-11-09 18:07:43 -0500

    Print Request data now, push to heroku

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # @Author: AnthonyKenny98
 # @Date:   2019-11-09 15:25:47
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2019-11-09 18:04:26
+# @Last Modified time: 2019-11-09 18:07:40
 
 # from src.groupme.groupme import GroupMe, GroupMeBot
 
@@ -20,7 +20,7 @@ def index():
 @app.route('/groupme/<randomint>')
 def call_bot(randomint):
     """Call Bot."""
-    print(request.data)
+    print(request.get_json())
     return jsonify({"Message": "Response"})
 
 if __name__ == '__main__':
