d6633b509d857a107338b9944a4210584c119ffe
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2019-11-09 17:17:42 -0500

    Adds URL for Callback Bot

--- src/app.py
+++ src/app.py
@@ -3,17 +3,22 @@
 # @Author: AnthonyKenny98
 # @Date:   2019-11-09 15:25:47
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2019-11-09 17:12:47
+# @Last Modified time: 2019-11-09 17:17:17
 
 from flask import Flask, jsonify  # , request
 app = Flask(__name__)
 
 
 @app.route('/')
-def respond():
+def index():
     """Basic Respond."""
     return jsonify({"Message": "Response"})
 
+
+@app.route('/callbot/<bot_id>')
+def call_bot(bot_id):
+    """Call Bot."""
+    return jsonify({"ID": bot_id})
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
