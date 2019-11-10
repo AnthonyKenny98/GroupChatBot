525f13a771f95873273f71953ca3d58a2551ba65
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2019-11-10 08:40:44 -0500

    Testing post to Heroku

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@
 # @Author: AnthonyKenny98
 # @Date:   2019-11-09 18:40:52
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2019-11-10 08:39:18
+# @Last Modified time: 2019-11-10 08:40:42
 
 
 from src.groupme.groupme import GroupMeBot, GroupMe
@@ -26,4 +26,4 @@ def handle(data):
     bot = GroupMeBot('4a8cf510b7541a8a3c96eb17a5')
 
     # Post Message from Bot
-    bot.post_message(bots)
+    bot.post_message(str(bots))
