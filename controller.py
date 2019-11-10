1c481d876437458219cf8c2af85d68e46905a4a9
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2019-11-09 19:03:27 -0500

    get bots

--- controller.py
+++ controller.py
@@ -3,10 +3,10 @@
 # @Author: AnthonyKenny98
 # @Date:   2019-11-09 18:40:52
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2019-11-09 19:02:19
+# @Last Modified time: 2019-11-09 19:03:24
 
 
-from src.groupme.groupme import GroupMeBot
+from src.groupme.groupme import GroupMeBot, GroupMe
 
 
 def handle(data):
