c8ee9908d37c5c2a1122f3c9dcb9df5a67bd71f1
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2019-11-11 20:43:07 -0500

    Implement Better Insults
    - New preambles
    - Basic construction of insult with probability

--- src/chatbot/chatbot.py
+++ src/chatbot/chatbot.py
@@ -4,7 +4,7 @@
 # @Author: AnthonyKenny98
 # @Date:   2019-11-11 13:31:40
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2019-11-11 20:16:24
+# @Last Modified time: 2019-11-11 20:37:43
 
 import json
 import os
@@ -71,13 +71,20 @@ class ChatBot:
         """Return a random phrase from vocab."""
         return random.choice(self.load_file('/vocab/phrases.txt'))
 
-    def insult(self):
+    def insult(self, tag_member=True):
         """Return a random insult built from vocab."""
-        noun = random.choice(self.load_file('/vocab/nouns.txt'))
-        adjective = random.choice(self.load_file('/vocab/adjectives.txt'))
-        preamble = random.choice(self.load_file('/vocab/preambles.txt'))
-        return self.tag_member() + ' ' + preamble + ' ' + adjective \
-            + ' ' + noun
+        message = ""
+        if self.decision_true():
+            message += self.tag_member() + ' '
+        if self.decision_true():
+            message += random.choice(self.load_file('/vocab/preambles.txt')) \
+                + ' '
+        if self.decision_true():
+            message += random.choice(self.load_file('/vocab/adjectives.txt')) \
+                + ' '
+        if self.decision_true():
+            message += random.choice(self.load_file('/vocab/nouns.txt'))
+        return message
 
     @staticmethod
     def load_file(file):
@@ -86,3 +93,8 @@ class ChatBot:
         with open(path + file, 'r') as f:
             list = f.read().splitlines()
         return list
+
+    @staticmethod
+    def decision_true(probability=0.5):
+        """Return True with given probability."""
+        return random.random() < probability
