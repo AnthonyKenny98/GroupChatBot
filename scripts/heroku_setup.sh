a5c48e5741a4d165e14dfd0c66311e632786c985
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2020-03-23 20:17:11 +1100

    Improve heroku setup

--- scripts/heroku_setup.sh
+++ scripts/heroku_setup.sh
@@ -1,7 +1,7 @@
 # @Author: AnthonyKenny98
 # @Date:   2019-11-09 15:16:36
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2020-03-23 19:28:22
+# @Last Modified time: 2020-03-23 19:59:57
 
 heroku login -i
 
@@ -10,7 +10,11 @@ read appName
 
 heroku create $appName --buildpack heroku/python
 
+git init .
+git add -A
+git commit -am "Heroku Commit"
 heroku git:remote -a $appName
+git push heroku master
 
 echo Please enter Reddit Client ID
 read RedditClientID
@@ -19,6 +23,3 @@ read RedditClientSecret
 
 heroku config:set RedditClientID=$RedditClientID
 heroku config:set RedditClientSecret=$RedditClientSecret
-
-git init .
-git push heroku master
